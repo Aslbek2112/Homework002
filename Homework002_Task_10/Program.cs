64296a087660745af717776b0842228215743b5e
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-06-19 15:56:09 +0300

    Homework002_Task10

---
+++ Homework002_Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+
+int number1 = number / 10;
+int number2 = number1 % 10;
+
+Console.WriteLine($"{number} -> {number2}");
